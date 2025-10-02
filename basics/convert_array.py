bc7e0d88545612ab5596b9a50800e15a263bb517
johnnas12 <yohanesmesfin3@gmail.com>
2025-10-02 10:32:26 +0300

    algo: convert the array the better approach using hashmap

--- basics/convert_array.py
+++ basics/convert_array.py
@@ -23,8 +23,31 @@ def convert(arr):
         arr[idx] = float("inf")
     return new_arr
 
+
+def convert_better(arr):
+    # creating temporary array and copy over all the elements of original lists
+    tmp = [arr[i] for i in range(len(arr))]
+    # use builtin sort function
+    tmp.sort()
+    # creating empty hash table 
+    umap = {}
+    # initializing the value increases as we go from left to right
+    val = 0
+    # loop through all elements and increament the val and assign the key(the original number) their order 
+    for i in range (len(tmp)):
+        umap[tmp[i]] = val
+        val += 1
+
+    # maping the hash value to arr it just replacing the values with their rank
+    for i in range (len(arr)):
+        arr[i] = umap[arr[i]]
+
+    return arr
+
 if __name__ == '__main__':
     arr = [10, 20, 15, 12, 11, 50]
     print(convert(arr))
+    test = [10, 20, 15, 12, 11, 50]
+    print(convert_better(test))
 
 
