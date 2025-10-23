0d2ea2dc141282a083f5688d286bdef51f22c06c
johnnas12 <yohanesmesfin3@gmail.com>
2025-10-23 20:29:26 +0300

    algo: next round implementation

---
+++ codeforces/next_round.py
@@ -0,0 +1,22 @@
+def next_round():
+    pass
+
+
+if __name__ == "__main__":
+    n = input()
+    splitted = n.split(" ")
+    my_n = int(splitted[0])
+    k = int(splitted[1])
+    
+    for _ in range(my_n):
+        scores = input()
+        scores_splitted = scores.split(" ")
+        break
+    print(scores_splitted)
+    
+    # print(my_n)
+    # print(target)
+
+    # for _ in range(n):
+        
+        
